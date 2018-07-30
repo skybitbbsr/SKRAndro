f8931803227b29733a5bac5bf87a5efce3bbe5de
Surya Kanta <surya.rath02@gmail.com>
2018-07-30 23:29:02 +0530

    30.07.18 commit from home
    Signed-off-by: Surya Kanta <surya.rath02@gmail.com>

---
+++ app/src/main/java/com/skybit/skrandro/Login.java
@@ -0,0 +1,99 @@
+package com.skybit.skrandro;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Login extends AppCompatActivity {
+    private static final String LOGIN_URL = "localhost/cetbusservice/login.php/";
+    EditText bus_number;
+    EditText password;
+    Button signin;
+    String result;
+    String[] params = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signin = (Button) findViewById(R.id.signin_button);
+        bus_number = (EditText) findViewById(R.id.user_field);
+        password = (EditText) findViewById(R.id.password_field);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+
+            }
+        });
+    }
+
+    private void loginUser() {
+        final String bus_no, pass;
+        bus_no = bus_number.getText().toString();
+        pass = password.getText().toString();
+        login(bus_no, pass);
+    }
+
+    private void login(String bus_no, String pass) {
+        final String url_suffix = "?busno=" + bus_no + "&pass=" + pass;
+        class LoginUser extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(Login.this, "Please wait...", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                if (result == null) {
+                    Toast.makeText(getApplicationContext(), "Network error occured!", Toast.LENGTH_SHORT).show();
+                } else if (result.equals("Login Failed!")) {
+                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                } else if (result.contains("Success")) {
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    startActivity(i);
+                }
+
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+                String s = params[0];
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(LOGIN_URL + url_suffix);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    result = bufferedReader.readLine();
+                    return result;
+
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        LoginUser lu = new LoginUser();
+        lu.execute(url_suffix);
+    }
+}
---
+++ app/src/main/java/com/skybit/skrandro/MainActivity.java
@@ -0,0 +1,122 @@
+package com.skybit.skrandro;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textview;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.share);
+        textview = (TextView) findViewById(R.id.text_view_main);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String output = "Lattitude: " + location.getLatitude() + "\n" + "Longitude: " + location.getLongitude();
+                textview.setText(output);
+                Toast.makeText(MainActivity.this, output, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+                }, 10);
+                return;
+            } else {
+                configureButton();
+            }
+        } else {
+            configureButton();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    private void configureButton() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    buildAlertMessageNoGps();
+
+                } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                }
+
+            }
+        });
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please Turn ON your GPS Connection")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
